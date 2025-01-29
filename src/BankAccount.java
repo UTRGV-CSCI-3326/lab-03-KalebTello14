1873c5ae92f00809e961de157c200ca52fb7326e
KalebTello14 <156832840+KalebTello14@users.noreply.github.com>
2025-01-29 19:40:54 +0000

    first bankcommit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,14 @@
+public class BankAccount {
+    public static void main(String[] args){
 
+        double checkingacct = 2175.37;
+        checkingacct -= 302.50;
+        checkingacct += 50.03;
+        checkingacct /= 2;
+        checkingacct += 20;
+        checkingacct -= 1;
+        checkingacct *= 2;
+        checkingacct += 1;
+        System.out.println("Your New Balance is: " + String.format("%.2f", checkingacct));
+    }
+}
